e30672610944dc19ec8eb944963e2e2be033623b
Yllnora Hoxha <yllnorahoxha@Mac.fritz.box>
2025-01-14 00:09:57 +0100

    Aktualisiert: Modell 'Reservation', URLs und Login-View

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
@@ -9,7 +8,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),  # Register-Route
     path('dashboard/', views.dashboard, name='dashboard'),  # Dashboard-Route
     path('configure/', views.configure, name='configure'),  # Configure-Route
-    path('success/', views.reservation_success, name='reservation_success'),
-    path('logout/', views.logout_view, name='logout')  # Logout mit Bestätigungsseite
-
+    path('success/', views.reservation_success, name='reservation_success'),  # Erfolgsseite
+    path('admin/reservations/', views.admin_reservations, name='admin_reservations'),  # Admin-Seite
+    path('logout/', views.logout_view, name='logout'),  # Logout-Route
+    path('reservations/', views.user_reservations, name='user_reservations'),  # Benutzerreservierungen
 ]
