780422ab31677065e06d53c29069a7a657dd780c
Yllnora <yllnora.hoxha@tha.de>
2025-01-14 21:44:59 +0100

    Update urls.py

--- studynest/urls.py
+++ studynest/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='reservations/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-    path('', include('reservations.urls')),  # Home, Login, Registrierung
+    path('', include('reservations.urls')),  
 ]
