ce946f7a1e7b322922c3b9b4c77dec645cc7f18b
Yllnora <yllnora.hoxha@tha.de>
2025-01-14 21:55:16 +0100

    Update utils.py

--- reservations/utils.py
+++ reservations/utils.py
@@ -6,7 +6,7 @@ def is_reservation_available(start_date, end_date):
     Prüft, ob eine Reservierung im angegebenen Zeitraum möglich ist.
     """
     conflicting_reservations = Reservation.objects.filter(
-        start_date__lt=end_date,  # Startdatum der bestehenden Reservierung ist vor dem Enddatum der neuen
-        end_date__gt=start_date   # Enddatum der bestehenden Reservierung ist nach dem Startdatum der neuen
+        start_date__lt=end_date,  
+        end_date__gt=start_date  
     )
-    return not conflicting_reservations.exists()  # Verfügbar, wenn keine Konflikte existieren
+    return not conflicting_reservations.exists()  
